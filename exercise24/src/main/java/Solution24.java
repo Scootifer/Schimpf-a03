b1873f3aefe5e0741fa6086f84b04715861de169
Scootifer <Windmill3341!>
2021-10-01 15:04:31 -0400

    solution 24 skeleton

---
+++ exercise24/src/main/java/Solution24.java
@@ -0,0 +1,10 @@
+public class Solution24 {
+    //The program will loop through the given word letter by letter using the String.contains() function
+    //If any letter is not contained the function will return false
+
+    boolean isAnagram(String word1, String word2);
+
+    public static void main(String[] args) {
+
+    }
+}
