7ac4f2fd8b4040a9d8dbc9f96c38bcc67820cea7
Scootifer <Windmill3341!>
2021-10-01 17:12:32 -0400

    Solution 30 skeleton

---
+++ exercise30/src/main/java/Solution30.java
@@ -0,0 +1,15 @@
+public class Solution30 {
+
+    //The program will use a nested loop to calculate each space of the table and print it using the printTable function
+
+    void printTable() {
+
+    }
+
+
+    public static void main(String[] args) {
+
+
+
+    }
+}
