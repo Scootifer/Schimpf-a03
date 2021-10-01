74641db675f33f3fc3c658e117471c7729a332f8
Scootifer <Windmill3341!>
2021-10-01 16:25:56 -0400

    Exercise 26 skeleton

---
+++ exercise26/src/main/java/PaymentCalculator.java
@@ -0,0 +1,6 @@
+public class PaymentCalculator {
+
+    // The PaymentCalculator will have a set function for each of it's values and then a public calculateMonthsUntilPaidOff
+    // calculateMonthsUntilPaidOff will use the class' variables to return an integer number of months until paid off.
+
+}
---
+++ exercise26/src/main/java/Solution26.java
@@ -0,0 +1,11 @@
+public class Solution26 {
+
+    // The program will accept the users input of  the, number of months, daily rate, balance, and monthly payment
+    // Then it will update the values of and use the class PaymentCalculator calling calculateMonthsUntilPaidOff to get the months remaining
+    //
+
+
+    public static void main(String[] args) {
+
+    }
+}
