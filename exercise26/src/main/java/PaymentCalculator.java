61d103c0b91d021dd51501fe845d201424397373
Scootifer <Windmill3341!>
2021-10-01 19:33:01 -0400

    Solution26

--- exercise26/src/main/java/PaymentCalculator.java
+++ exercise26/src/main/java/PaymentCalculator.java
@@ -1,6 +1,24 @@
+import java.lang.Math;
 public class PaymentCalculator {
 
-    // The PaymentCalculator will have a set function for each of it's values and then a public calculateMonthsUntilPaidOff
+    // The PaymentCalculator will have a set its values through the constructor and then use a public calculateMonthsUntilPaidOff method
     // calculateMonthsUntilPaidOff will use the class' variables to return an integer number of months until paid off.
+
+    double dRate;
+    double balance;
+    double monthlyPayment;
+
+    public PaymentCalculator(double i, double b, double m) {
+
+        this.dRate = (i/100)/365;
+        this.balance = b;
+        this.monthlyPayment = m;
+    }
+
+    public double calculateMonthsUntilPaidOff() {
+
+
+        return Math.ceil(-(1/30) * Math.log(1 + (this.balance/this.monthlyPayment) * (1 - Math.pow((1+this.dRate), 30))) / Math.log(1 + this.dRate));
+    }
 
 }
--- exercise26/src/main/java/Solution26.java
+++ exercise26/src/main/java/Solution26.java
@@ -1,11 +1,34 @@
+import java.util.Scanner;
+
 public class Solution26 {
 
-    // The program will accept the users input of  the, number of months, daily rate, balance, and monthly payment
+    // The program will accept the users input of  the, number of months, daily rate, balance, and monthly payment in the constructor
     // Then it will update the values of and use the class PaymentCalculator calling calculateMonthsUntilPaidOff to get the months remaining
     //
 
-
     public static void main(String[] args) {
+        final Scanner scan = new Scanner(System.in);
+
+        double balance, dRate, monthlyPayment;
+        double numMonths;
+
+
+        System.out.print("Please enter the balance: ");
+        balance = Double.parseDouble(scan.nextLine());
+
+        System.out.print("Please enter the APR: ");
+        dRate = Double.parseDouble(scan.nextLine());
+
+        System.out.print("Please enter the monthly payments: ");
+        monthlyPayment = Double.parseDouble(scan.nextLine());
+
+        final PaymentCalculator calc = new PaymentCalculator(dRate, balance, monthlyPayment);
+
+        numMonths = calc.calculateMonthsUntilPaidOff();
+
+        System.out.println("It will take " + numMonths + " months to pay the card off.");
+
+
 
     }
 }
