cacd448229a9515b4a689ca2c16e166faa57e773
Scootifer <Windmill3341!>
2021-10-01 17:28:07 -0400

    Solution31 Skeleton

---
+++ exercise31/src/main/java/HeartRateManager.java
@@ -0,0 +1,13 @@
+public class HeartRateManager {
+
+    //This class will store the resting pulse and age of the user and have set functions accordingly
+    // The class will have a printTableHead() function which will do such, and then a printTableData() function to fill the rest of the table
+
+    void printTableHead() {}
+
+    void printTable() {}
+
+    void setResting (int rp) {}
+
+    void setAge (int a) {}
+}
---
+++ exercise31/src/main/java/Solution31.java
@@ -0,0 +1,9 @@
+public class Solution31 {
+
+    //The program will use the HeartRateManager class to handle print statements and calculations
+
+    public static void main(String[] args) {
+
+    }
+
+}
