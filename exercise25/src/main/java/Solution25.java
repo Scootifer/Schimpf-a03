41fafc4d4a7c89d15e4a2e587d67f8a1353a4c04
Scootifer <Windmill3341!>
2021-10-01 16:12:02 -0400

    Solution25 skeleton

---
+++ exercise25/src/main/java/Solution25.java
@@ -0,0 +1,11 @@
+public class Solution25 {
+
+    //The program will accept user input and loop through each character, making a hasNumber or hasLetter or hasSpecial boolean
+    //The program will then match the corresponding length and attributes to find the "score" of the password, 0 for indeterminite, and {1,2,3,4} accoridngly
+
+    int passwordValidator(String password);
+
+    public static void main(String args[]) {
+
+    }
+}
