3307e47c1afafa172d8961300320c2748c93ca61
Scootifer <Windmill3341!>
2021-10-01 18:27:54 -0400

    Solution32 skeleton

---
+++ exercise32/src/main/java/GameManager.java
@@ -0,0 +1,17 @@
+public class GameManager {
+
+    // The class will use the setDifficulty function to set the difficulty and secret number
+    // The guess function will accept a guess and compare it to the secret number
+    // -it will determine and print output accordingly
+
+    private int difficulty;
+
+    public void setDifficulty(int d) {
+
+    }
+
+    public int guess(int guess) {
+
+        return 0;
+    }
+}
---
+++ exercise32/src/main/java/Solution32.java
@@ -0,0 +1,10 @@
+public class Solution32 {
+
+    //The program will use a GameManager class to handle the game data storage and functionality
+    // This program will use a do while loop to determine if the game will be played again
+
+     public static void main(String args) {
+
+     }
+
+}
