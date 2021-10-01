1ee21b14512d7ae0df2412e23fbb6f6d7c88e7c5
Scootifer <Windmill3341!>
2021-10-01 16:36:48 -0400

    Solution27 Skeleton

---
+++ exercise27/src/main/java/InputValidator.java
@@ -0,0 +1,34 @@
+public class InputValidator {
+
+    //The validateInput function will run the individual validateFName, validateLName, validateZip, and validateID functions
+    //if all functions return 0 then the inputs are valid and the program will prompt the user and terminate
+
+    private String fName;
+    private String lName;
+    private String ip;
+    private String id;
+
+    //Will ensure that there is input and that it is alphabetic
+    private int validateFName() {
+
+        return 0;
+    }
+
+    //Will ensure that there is input and that it is alphabetic
+    private int validateLName() {
+
+        return 0;
+    }
+
+    //Will ensure that input is a valid length and that it is numerical
+    private int validateZip() {
+
+        return 0;
+    }
+
+    //Will ensure that input is the correct format by checking each location
+    private int validateID() {
+
+        return 0;
+    }
+}
---
+++ exercise27/src/main/java/Solution27.java
@@ -0,0 +1,4 @@
+public class Solution27 {
+
+    //The program will use a class InputValidator to store the input and various verification functions
+}
