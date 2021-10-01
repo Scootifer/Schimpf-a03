9975c153a82e53635e2400806e57c3f34c65cdff
Scootifer <Windmill3341!>
2021-10-01 16:48:37 -0400

    Solution28 Skeleton

---
+++ exercise28/src/main/java/Solution28.java
@@ -0,0 +1,16 @@
+public class Solution28 {
+    //  The program will have a getInput function that will return the inputted integer
+    //  The getAddition function will loop calling the getInput function 5 times, adding the returned value and returning the final total variable
+
+    int getInput(){
+
+    }
+
+    int getAddition() {
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
