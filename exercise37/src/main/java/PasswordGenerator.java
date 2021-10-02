3d6b12e86833c6d6f665a18211b545ef0e3b1074
Scootifer <Windmill3341!>
2021-10-02 16:49:22 -0400

    Solution37 Skeleton

---
+++ exercise37/src/main/java/PasswordGenerator.java
@@ -0,0 +1,22 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Scott Schimpf
+ */
+
+// The PasswordGenerator class will store the possible character sets as final lists
+// The class will validate that the password character count constraints are met
+//
+
+public class PasswordGenerator {
+
+    //will return 0 for valid, or -1 for invalid character count;
+    public int validate(String p) {
+        return 0;
+    }
+
+    public String generatePassword(){
+        String password;
+
+        return password;
+    }
+}
---
+++ exercise37/src/main/java/Solution37.java
@@ -0,0 +1,18 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Scott Schimpf
+ */
+
+// This program will use the PasswordGenerator class to handle making the password and storing possible password characters
+// The Solution37 class will interact with the PasswordGenerator and prompt the user as need be.
+//
+
+public class Solution37 {
+
+
+
+    public static void main(String[] args ) {
+
+
+    }
+}
