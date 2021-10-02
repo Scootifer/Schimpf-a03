1a48791e53208400607ca7f99d3e03c5cbe849d7
Scootifer <Windmill3341!>
2021-10-02 16:39:42 -0400

    Solution 36

--- exercise36/src/main/java/Calculator.java
+++ exercise36/src/main/java/Calculator.java
@@ -5,7 +5,9 @@
 
 // the calculator class will have the functions max, min, average, and std
 // values will be added to the local arraylist through the addValue function
+// input will be validated with validate input
 
+import java.lang.Math;
 import java.util.ArrayList;
 public class Calculator {
 
@@ -32,23 +34,61 @@ public class Calculator {
     // max will set a max variable as the first index of the list and loops through the list reassigning the value to anything larger
     // assumes the list is not empty as it is not specified to arrange for such.
     public int max() {
-        return 0;
+        int max = this.list.get(0);
+
+        for(int i: this.list) {
+            if (i > max) {
+                max = i;
+            }
+        }
+
+        return max;
     }
 
     // min will set a max variable as the first index of the list and loops through the list reassigning the value to anything smaller
     // assumes the list is not empty as it is not specified to arrange for such.
     public int min() {
-        return 0;
+
+        int min = this.list.get(0);
+
+        for(int i: this.list) {
+            if (i < min) {
+                min = i;
+            }
+        }
+
+        return min;
     }
 
     //std will take advantage of the average function to gather the mean of the group. Then iterate the list adding the standard deviation of each num
-    //std = Sum(number[s] - average)^2 / len
+    //std = sqrt(Sum(number[s] - average)^2 / len)
     public double std() {
+        double sd = 0.0;
+        double average = this.average();
 
-
-        return 0.0;
+        for (int i : this.list) {
+            sd += Math.pow(i - average, 2);
+        }
+        return Math.sqrt(sd / this.list.size() );
     }
 
+    // validateInput
+    //  Accepts the input and what input type ('i', 'd') the input should be.
+    //  returns 0 for invalid and 1 for valid
+
+    public int validateInput(String input, char type){
+
+        if(type == 'i' ) {
+            try{
+                Integer.parseInt(input);
+            }
+            catch(Exception e){
+                return -1;
+            }
+        }
+
+        return 0;
+    }
 
 
 }
--- exercise36/src/main/java/Solution36.java
+++ exercise36/src/main/java/Solution36.java
@@ -6,9 +6,43 @@
 // The math and related functions will be handled by the Calculator class
 // The solution46 class will use a do while loop to continue until a 'd' is detected in the first spot of input.
 
+import java.util.Scanner;
+
 public class Solution36 {
 
     public static void main(String[] args) {
+
+        boolean run = true;
+        String input;
+        char c;
+
+        final Scanner scan = new Scanner(System.in);
+        Calculator calc = new Calculator();
+
+
+
+        do
+        {
+            System.out.print("Please enter a number (enter done to quit): ");
+            input = scan.nextLine();
+
+            if (calc.validateInput(input, 'i') == 0) {
+                calc.addValue(Integer.parseInt(input));
+            }
+            else if (input.equalsIgnoreCase("done")){
+                run = false;
+            }
+            else {
+                System.out.println("Please enter an integer value");
+            }
+
+        }while(run);
+
+        System.out.println();
+        System.out.println("The Average of the data is: " + calc.average());
+        System.out.println("The Maximum of the data is: " + calc.max());
+        System.out.println("The Minimum of the data is: " + calc.min());
+        System.out.println("The Standard Deviation of the data is: " + calc.std());
 
 
     }
