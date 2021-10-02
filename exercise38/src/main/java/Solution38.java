1e1f721fbc7e009d3afed3cbb9f6531f49d17f56
Scootifer <Windmill3341!>
2021-10-02 18:27:08 -0400

    Solution38 Skeleton

---
+++ exercise38/src/main/java/Solution38.java
@@ -0,0 +1,22 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Scott Schimpf
+ */
+
+// This program will use a filterEvenNumbers that will go piece by piece of the array and check if the value mod 2 = 0
+// if it is it will be added to the new array to later be returned
+// The list will be created using nextInt() and next() to filter spaces between numbers.
+
+public class Solution38 {
+
+    int[] filterEvenNumbers(int[] list) {
+        int[] ret = new int[list.length];
+
+
+        return ret;
+    }
+
+    static void main(String[] args) {
+
+    }
+}
