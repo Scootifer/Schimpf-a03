49185f911c28de5be4fb1f8a903a015c5770001b
Scootifer <Windmill3341!>
2021-10-01 20:03:15 -0400

    Solution27

--- exercise27/src/main/java/InputValidator.java
+++ exercise27/src/main/java/InputValidator.java
@@ -5,11 +5,52 @@ public class InputValidator {
 
     private String fName;
     private String lName;
-    private String ip;
+    private String zip;
     private String id;
+
+    public InputValidator(String f, String l, String z, String i) {
+        this.fName = f;
+        this.lName = l;
+        this.zip = z;
+        this.id = i;
+    }
+
+    public int validateInput() {
+
+        int ret = 0;
+
+        if(validateFName() != 0) {
+            ret = -1;
+        }
+        if (validateLName() != 0){
+            ret = -1;
+        }
+        if (validateZip() != 0){
+            ret = -1;
+        }
+        if (validateID() != 0){
+            ret = -1;
+        }
+        return ret;
+    }
 
     //Will ensure that there is input and that it is alphabetic
     private int validateFName() {
+
+        int len = this.fName.length();
+
+        if (len < 2) {
+            System.out.println("First Name must be 2 letters or more.");
+            return -1;
+        }
+
+        for(int i =0; i<len; i++) {
+            char c = this.fName.charAt(i);
+            if (!((c > 64) && (c < 91))  &&  !((c > 96) && (c < 123))) {
+                System.out.println("Please enter letters only.");
+                return -1;
+            }
+        }
 
         return 0;
     }
@@ -17,17 +58,81 @@ public class InputValidator {
     //Will ensure that there is input and that it is alphabetic
     private int validateLName() {
 
+        int len = this.lName.length();
+
+        if (len < 2) {
+            System.out.println("Last Name must be 2 letters or more.");
+            return -1;
+        }
+
+        for(int i =0; i<len; i++) {
+            char c = this.lName.charAt(i);
+            if (!((c > 64) && (c < 91))  &&  !((c > 96) && (c < 123))) {
+                System.out.println("Please enter letters only.");
+                return -1;
+            }
+        }
+
         return 0;
     }
-
     //Will ensure that input is a valid length and that it is numerical
     private int validateZip() {
+
+        if(this.zip.length() != 5) {
+            System.out.println("Please enter 5 digits");
+            return -1;
+        }
+
+        for(int i =0; i< 5; i++) {
+            char c = this.zip.charAt(i);
+            if( !((c > 47) && (c < 58)) ) {
+                System.out.println("Please enter numbers for zipcode.");
+                return -1;
+            }
+        }
 
         return 0;
     }
 
     //Will ensure that input is the correct format by checking each location
     private int validateID() {
+        char c;
+        int len = this.id.length();
+
+        if(len != 7) {
+            System.out.println("Please enter ID in the format AA-1234.");
+            return -1;
+        }
+
+        for(int i =0; i< len; i++) {
+            c = this.id.charAt(i);
+
+            if (i == 0 || i == 1) {
+                if ( !((c > 64) && (c < 91))  &&  !((c > 96) && (c < 123))) {
+                    System.out.println("Please enter ID in the format AA-1234.");
+                    return -1;
+                }
+            }
+            else if (i == 2) {
+                if (c != 45) {
+                    System.out.println("Please enter ID in the format AA-1234.");
+                    return -1;
+                }
+            }
+            else {
+                if (!((c > 47) && (c < 58))) {
+                    System.out.println("Please enter ID in the format AA-1234.");
+                    return -1;
+                }
+
+            }
+        }
+
+
+
+
+
+
 
         return 0;
     }
--- exercise27/src/main/java/Solution27.java
+++ exercise27/src/main/java/Solution27.java
@@ -1,4 +1,29 @@
+import java.util.Scanner;
 public class Solution27 {
 
     //The program will use a class InputValidator to store the input and various verification functions
+
+    public static void main(String[] args) {
+        final Scanner scan = new Scanner(System.in);
+
+        String fName, lName, zip, id;
+
+        System.out.print("Please enter your first name: ");
+        fName = scan.nextLine();
+
+        System.out.print("Please enter your last name: ");
+        lName = scan.nextLine();
+
+        System.out.print("Please enter your zip: ");
+        zip = scan.nextLine();
+
+        System.out.print("Please enter your id: ");
+        id = scan.nextLine();
+
+        final InputValidator v = new InputValidator(fName, lName, zip, id);
+        if(v.validateInput() == 0) {
+            System.out.println("No errors found.");
+        }
+    }
+
 }
