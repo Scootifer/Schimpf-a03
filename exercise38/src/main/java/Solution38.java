6b2a5e189498d8fb279a4e1fa18cc344843b654e
Scootifer <Windmill3341!>
2021-10-02 19:02:35 -0400

    Solution38

--- exercise38/src/main/java/Solution38.java
+++ exercise38/src/main/java/Solution38.java
@@ -7,16 +7,51 @@
 // if it is it will be added to the new array to later be returned
 // The list will be created using nextInt() and next() to filter spaces between numbers.
 
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.List;
+import java.util.ArrayList;
 public class Solution38 {
 
-    int[] filterEvenNumbers(int[] list) {
-        int[] ret = new int[list.length];
+    List<Integer> filterEvenNumbers(List<Integer> list) {
 
+        int space = 0; //space in the new array to next occupy
+
+        List<Integer> ret = new ArrayList<>();
+
+        for(int x : list) {
+            if(x%2 == 0) {
+                ret.add(x);
+            }
+        }
 
         return ret;
     }
 
-    static void main(String[] args) {
+    public static void main(String[] args) {
+        final Scanner scan = new Scanner(System.in).useDelimiter(" ");
+        final Solution38 obj = new Solution38();
+
+
+
+        System.out.print("Please enter a list of numbers separated by spaces: ");
+
+        String input = scan.nextLine();
+        String[] nums = input.split(" ");
+
+        List<Integer> list = new ArrayList<>();
+
+        for (String num : nums) {
+            list.add(Integer.parseInt(num));
+        }
+
+        List<Integer> newList = obj.filterEvenNumbers(list);
+
+        System.out.println();
+        System.out.println("The list of only even numbers is");
+        for (Integer integer : newList) {
+            System.out.print(integer + " ");
+        }
 
     }
 }
