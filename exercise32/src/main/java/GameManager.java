32af544474c6e93672f5c3d68309fa868fbf9c45
Scootifer <Windmill3341!>
2021-10-01 23:13:19 -0400

    Solution32

--- exercise32/src/main/java/GameManager.java
+++ exercise32/src/main/java/GameManager.java
@@ -1,16 +1,47 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Scott Schimpf
+ */
+import java.util.Random;
 public class GameManager {
 
-    // The class will use the setDifficulty function to set the difficulty and secret number
+    // The class will use the constructor to set the secret number
     // The guess function will accept a guess and compare it to the secret number
     // -it will determine and print output accordingly
 
-    private int difficulty;
+    private int secretNumber;
 
-    public void setDifficulty(int d) {
+    public GameManager(int d) {
+        Random rand = new Random();
 
+        if(d == 1) {
+            this.secretNumber = 1 + rand.nextInt(10);
+        }
+        else if(d == 2) {
+            this.secretNumber = 1 + rand.nextInt(100);
+        }
+        else {
+            this.secretNumber = 1 + rand.nextInt(1000);
+        }
     }
 
+
+
+
     public int guess(int guess) {
+
+        if(guess == this.secretNumber) {
+            return 1;
+        }
+        else if (guess > this.secretNumber) {
+            System.out.println("Your guess is too high");
+        }
+        else if (guess < 0) {
+            System.out.println("Negative numbers and Characters are not valid.");
+        }
+        else {
+            System.out.println("Your guess is too low.");
+        }
 
         return 0;
     }
--- exercise32/src/main/java/Solution32.java
+++ exercise32/src/main/java/Solution32.java
@@ -1,9 +1,75 @@
-public class Solution32 {
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Scott Schimpf
+ */
+
+import java.util.Scanner;
+
+class Solution32 {
+
+     // validateInput
+     //  Accepts the input and what input type ('i') the input should be.
+     //  returns -1 for invalid and 0 for valid
+     int validateInput(String input, char type){
+
+
+
+          if(type == 'i' ) {
+               try{
+                    Integer.parseInt(input);
+               }
+               catch(Exception e){
+                    System.out.println("Please enter an integer.");
+                    return -1;
+               }
+
+          }
+
+          return 0;
+     }
 
     //The program will use a GameManager class to handle the game data storage and functionality
     // This program will use a do while loop to determine if the game will be played again
 
-     public static void main(String args) {
+     public static void main(String[] args) {
+
+          final Scanner scan = new Scanner(System.in);
+
+          final Solution32 obj = new Solution32();
+
+          String input;
+          int difficulty;
+          int guess;
+
+          do {
+               System.out.print("Please enter the difficulty: ");
+               input = scan.nextLine();
+               if(obj.validateInput(input, 'i') == 0) {
+                    difficulty = Integer.parseInt(input);
+               }
+               else {
+                    difficulty = 0;
+               }
+
+          }while(difficulty == 0);
+
+          final GameManager manager = new GameManager(difficulty);
+          int guessCount =0;
+
+          do {
+               System.out.print("Please enter your guess: ");
+               input = scan.nextLine();
+               if (obj.validateInput(input, 'i') == 0) {
+                    guess = Integer.parseInt(input);
+               }
+               else {
+                    guess = -1; //essentially skips this turn
+               }
+               guessCount++;
+
+          }while(manager.guess(guess) != 1);
+
+          System.out.println("You finished in " + guessCount + " guesses.");
 
      }
 
