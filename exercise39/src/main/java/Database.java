e24f3e053c692796bf5611351f3f46eaff155825
Scootifer <Windmill3341!>
2021-10-02 19:33:21 -0400

    Solution39 Skeleton

---
+++ exercise39/src/main/java/Database.java
@@ -0,0 +1,15 @@
+
+// The program will then use the native sorting of a TreeMap to sort the (lName, fname) map by LastName and from their use the lastName value to get the
+// corresponding values in the other maps.
+// (key:value) 1: (LastName, FirstName) 2:(LastName, Position) 3(LastName, separationDate)
+
+import java.util.*;
+public class Database {
+    private Map<String, String> fName = new HashMap<>();
+    private Map<String, String> position = new HashMap<>();
+    private Map<String, String> separationDate = new HashMap<>();
+
+    public Database() {
+        this.fName.put()
+    }
+}
---
+++ exercise39/src/main/java/Solution39.java
@@ -0,0 +1,11 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Scott Schimpf
+ */
+
+// The program Solution39 will use the Database class to employ 4 hash maps
+// (key:value) 1: (LastName, FirstName) 2:(LastName, Position) 3(LastName, separationDate) 4: (idNum, LastName)
+//
+import java.util.*;
+public class Solution39 {
+}
