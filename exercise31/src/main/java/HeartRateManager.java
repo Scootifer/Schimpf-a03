91e6a8cf0301ce7a02fa8e07825d6599388e5915
Scootifer <Windmill3341!>
2021-10-01 20:46:45 -0400

    Solution31

--- exercise31/src/main/java/HeartRateManager.java
+++ exercise31/src/main/java/HeartRateManager.java
@@ -1,13 +1,39 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Scott Schimpf
+ */
 public class HeartRateManager {
 
-    //This class will store the resting pulse and age of the user and have set functions accordingly
+    //This class will store the resting pulse and age of the user and set them in the constructor
     // The class will have a printTableHead() function which will do such, and then a printTableData() function to fill the rest of the table
+    //Using the calculateTargetHR function;
 
-    void printTableHead() {}
+    int resting, age;
 
-    void printTable() {}
+    public HeartRateManager(int r, int a) {
+        this.resting = r;
+        this.age = a;
+    }
 
-    void setResting (int rp) {}
+    void printTableHead() {
+        System.out.println();
+        System.out.println("Resting Pulse: " + this.resting + " \t Age: " + this.age);
+        System.out.println();
+        System.out.println("Intensity| Rate");
+        System.out.println("---------|------");
+    }
 
-    void setAge (int a) {}
+    void printTable() {
+        for(int i = 55; i<100; i+=5) {
+            System.out.println(i + "% \t | " + (int)this.calculateTargetHR(i) + " bpm");
+        }
+    }
+
+
+    //between using Math.ceil and not I had inconsistent results as the example and such do not know which to use
+    //Not using it has the most consistent results. It is not specified in the question
+    double calculateTargetHR(int percent) {
+
+        return ( ((220-this.age) - this.resting) * ((double)percent/100) + this.resting);
+    }
 }
--- exercise31/src/main/java/Solution31.java
+++ exercise31/src/main/java/Solution31.java
@@ -1,9 +1,28 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Scott Schimpf
+ */
+
+import java.util.Scanner;
 public class Solution31 {
 
     //The program will use the HeartRateManager class to handle print statements and calculations
 
     public static void main(String[] args) {
+        final Scanner scan = new Scanner(System.in);
 
+        int resting, age;
+
+        //input is handled this way to continously move the scanner to the next line
+        System.out.print("Please enter your resting heart rate: " );
+        resting = Integer.parseInt(scan.nextLine());
+
+        System.out.print("Please enter your age: " );
+        age = Integer.parseInt(scan.nextLine());
+
+        final HeartRateManager manager = new HeartRateManager(resting, age);
+        manager.printTableHead();
+        manager.printTable();
     }
 
 }
