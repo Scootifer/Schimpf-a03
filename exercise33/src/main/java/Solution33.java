96eb399325acb23fc8170ab3291e3f9ae8b801e2
Scootifer <Windmill3341!>
2021-10-02 12:46:10 -0400

    Solution33

--- exercise33/src/main/java/Solution33.java
+++ exercise33/src/main/java/Solution33.java
@@ -1,10 +1,17 @@
+import java.util.Scanner;
 public class Solution33 {
 
     //This program will use a class GameManager to handle the input and output.
     // I forgot to commit just a shell of this program. I swear I did it first
 
     public static void main(String[] args) {
-        final GameManager
+        final GameManager manager = new GameManager();
+        final Scanner scan = new Scanner(System.in);
+
+        System.out.print("What is your question: ");
+        scan.nextLine();
+        System.out.println();
+        System.out.println(manager.pickRand());
 
     }
 }
