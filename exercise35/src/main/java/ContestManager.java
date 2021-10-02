e4aa1c62128e1baa4b6b30c6e27c265372c04d62
Scootifer <Windmill3341!>
2021-10-02 13:17:21 -0400

    Solution35 Skeleton

---
+++ exercise35/src/main/java/ContestManager.java
@@ -0,0 +1,10 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Scott Schimpf
+ */
+
+
+//The ContestManager will have an addPlayer function to add people to the list, and a pickWinner function that will use random to pick from the list.
+
+public class ContestManager {
+}
---
+++ exercise35/src/main/java/Solution35.java
@@ -0,0 +1,10 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Scott Schimpf
+ */
+
+// This program will use a ContestManager class that will handle the game
+// the Solution35 class will have a do while loop continuing until input is empty
+
+public class Solution35 {
+}
