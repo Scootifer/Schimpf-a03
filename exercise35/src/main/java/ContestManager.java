1a5f032a53dbe07e070c7110855cc3c7b99ac698
Scootifer <Windmill3341!>
2021-10-02 13:45:42 -0400

    Solution35

--- exercise35/src/main/java/ContestManager.java
+++ exercise35/src/main/java/ContestManager.java
@@ -6,5 +6,19 @@
 
 //The ContestManager will have an addPlayer function to add people to the list, and a pickWinner function that will use random to pick from the list.
 
+import java.util.Random;
+import java.util.ArrayList;
 public class ContestManager {
+
+    private ArrayList<String> list = new ArrayList<>();
+
+    public void addPerson(String name) {
+        this.list.add(name);
+    }
+
+    public String pickWinner() {
+        Random rand = new Random();
+        return list.get(rand.nextInt(list.size()));
+    }
+
 }
--- exercise35/src/main/java/Solution35.java
+++ exercise35/src/main/java/Solution35.java
@@ -6,5 +6,34 @@
 // This program will use a ContestManager class that will handle the game
 // the Solution35 class will have a do while loop continuing until input is empty
 
+import java.util.Scanner;
 public class Solution35 {
+
+
+
+    public static void main(String[] args) {
+        boolean run = true;
+        String input;
+
+        final Scanner scan = new Scanner(System.in);
+        ContestManager manager = new ContestManager();
+
+
+
+        do
+        {
+            System.out.print("Please enter a user (enter a space to quit): ");
+            input = scan.nextLine();
+            if(!input.equals(" ") && input.length() > 0) {
+                manager.addPerson(input);
+            }
+            else {
+                run = false;
+            }
+
+        }while(run);
+        System.out.println();
+        System.out.println("The winner is " + manager.pickWinner());
+    }
+
 }
