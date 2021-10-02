07b7762afd97891f34e45d9d5b9577d091349fcd
Scootifer <Windmill3341!>
2021-10-01 20:19:24 -0400

    Solution29

--- exercise29/src/main/java/Solution29.java
+++ exercise29/src/main/java/Solution29.java
@@ -1,3 +1,10 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 first_name last_name
+*/
+
+import java.util.Scanner;
+
 public class Solution29 {
 
     //The program will use the function validateInput to validate input and calculateTime to perform the math
@@ -7,22 +14,42 @@ public class Solution29 {
     //  returns -1 for invalid and 0 for valid
     int validateInput(String input, char type){
 
+        int i;
 
         if(type == 'i' ) {
             try{
-                Integer.parseInt(input);
+                i = Integer.parseInt(input);
             }
             catch(Exception e){
-                System.out.println("Please enter an integer value");
-                return 0;
+                System.out.println("Sorry that's not valid.");
+                return -1;
+            }
+            if(i == 0) {
+                System.out.println("Sorry you can't enter 0.");
+                return -1;
             }
         }
 
-
-        return 1;
+        return 0;
     }
 
     public static void main(String[] args) {
+
+        final Solution29 obj = new Solution29();
+        final Scanner scan = new Scanner(System.in);
+        boolean runLoop = true;
+        String input;
+
+        do {
+            System.out.print("Please enter the rate of return: ");
+            input = scan.nextLine();
+            if (obj.validateInput(input, 'i') == 0) {
+                runLoop = false;
+            }
+        }while(runLoop);
+
+        System.out.println("It will take " + 72/Integer.parseInt(input) + " years to double your initial investment.");
+
 
 
     }
